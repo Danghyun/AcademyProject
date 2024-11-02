d50cf7b78de6ba7cfcffe3eb8933aa1d2ce1558b
Danghyun <daehyun0115@naver.com>
2024-11-02 21:09:14 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:project/util/index.dart';
 import 'package:window_manager/window_manager.dart';
 
 void main() async {
+  // 깃허브 데스크탑 테스트
   // Flutter 에서 앱이 실행되기 전에 필요한 Flutter 엔진 및 위젯 바인딩을 초기화해 주는 역할임.
   // 이 메서드를 호출 함으로써, Flutter의 기본적인 위젯 시스템과 엔진이 모두 초기화된 상태를 보장하여
   // 앱이 정상적으로 실행이 될 수 있게 함.
